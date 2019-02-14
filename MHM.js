41ee3a02ee4b2c0108058f9106c4f4032ab5279b
Venom (Mel Macaluso) <capeeshe@gmail.com>
2019-02-14 18:25:06 +0000

    actually checking if we got snappers or togglers

--- MHM.js
+++ MHM.js
@@ -7,11 +7,11 @@ import {MHM_items} from './functions/MHM_items'
     console.info('%c[MHM Utility Framework:'+'%c Loaded'+'%c]','color:blue;','color:green; font-weight:bold;','color:blue;')
     //Utilities, to be executed when the window has loaded
     window.addEventListener('load', ()=> {
-        if(MHM_items().togglers) {
+        if(MHM_items().togglers.length) {
             MHM_toggler()
         }
-        if(MHM_items().snappers) {
-        MHM_snapper()
+        if(MHM_items().snappers.length) {
+            MHM_snapper()
         }
     })
 })()
