b3f1d4d5db9325ef5d55d4a5ce2713d8cc0155cc
Venom (Mel Macaluso) <capeeshe@gmail.com>
2019-02-14 17:08:45 +0000

    minifying main file

--- MHM.js
+++ MHM.js
@@ -4,7 +4,6 @@ import {MHM_snapper} from './functions/MHM_snapper'
 (()=>{
     // Init message
     console.info('%c[MHM Utility Framework:'+'%c Loaded'+'%c]','color:blue;','color:green; font-weight:bold;','color:blue;')
-
     //Utilities, to be executed when the window has loaded
     window.addEventListener('load', ()=> {
         MHM_toggler()
